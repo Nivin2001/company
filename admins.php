fb8a66a6f3ae2d9c6b7da2eb738a5d87ee203900
nivin shabat <nivin20012001@gmail.com>
2022-06-26 22:58:24 +0300

    show admins

--- admins.php
+++ admins.php
@@ -1,6 +1,19 @@
 <?php
 require('header.php');
 ?>
+<?php
+require('connection.php');
+$query="SELECT * FROM admin";
+$result=mysqli_query($conn,$query);
+if(mysqli_num_rows($result)>0){//اذا كان اكتر من سطر
+    $admins=mysqli_fetch_all($result,MYSQLI_ASSOC); // ترجعلي البيانات في assoc بدلا من index
+}else{
+        $msg="no data found";
+      
+    }
+    
+?>
+
 
     <div class="container-fluid py-5">
         <div class="row">
@@ -9,7 +22,9 @@ require('header.php');
 
                 <div class="d-flex justify-content-between align-items-center mb-3">
                     <h3>All Admins</h3>
+                    <a heef=# class="btn btn-success"> add admin </a>
                 </div>
+                
 
                 <table class="table table-hover">
                     <thead>
@@ -17,14 +32,40 @@ require('header.php');
                         <th scope="col">#</th>
                         <th scope="col">Name</th>
                         <th scope="col">Email</th>
+                        <th scope="col">Status</th>
                         <th scope="col">Actions</th>
                       </tr>
                     </thead>
                     <tbody>
+                       
+                    <?php  
+                    if(!empty($admins)):
+                      foreach($admins as $index=>$admin):  
+                     // اسم المصفوفة admins 
+                    //حولتها ل assoc
+                    //as admin
+                    //بطبع محتويات المصفوفة بعدها
+                    //<?= بدل echo
+                    // المعلومات انطبعت بدون م يدخل ع php mu admin 
+                    //index يطبع من 1
+                    // لازم يكون في شرط  ميشان يدخل ع foreach
+                        ?>
                       <tr>
-                        <th scope="row">1</th>
-                        <td>Kareem Fouad</td>
-                        <td>kareem@techstore.com</td>
+                        <th scope="row"><?= $index +1 ?></th>
+                        <td><?= $admin['name']?></td>
+                        <td> <?= $admin['email']?></td>
+                        <td> <?php 
+                        if ($admin['status']==1):?>
+                        <i class="fas fa-check"></i>
+                         <?php
+                        else:?>
+                       <span class="badge badge-danger">
+                       <i class="fas fa-times"></i>
+                        </span>
+                          
+                            <?php endif;
+                        ?></td>
+
                         <td>
                             <a class="btn btn-sm btn-info" href="#">
                                 <i class="fas fa-edit"></i>
@@ -34,6 +75,11 @@ require('header.php');
                             </a>
                         </td>
                       </tr>
+                      <?php endforeach;
+                      else:
+                        echo $msg;
+                      endif;
+                      ?>
                     </tbody>
                 </table>
             </div>
@@ -41,3 +87,4 @@ require('header.php');
         </div>
     </div>
     <?php require_once('footer.php');?>
+    
--- index.php
+++ index.php
@@ -1,6 +1,12 @@
 <?php
 require('header.php');
 ?>
+<?php
+require('connection.php');
+$query="SELECT COUNT(id) as admincount FROM admin";
+$result=mysqli_query($conn,$query);// تنفيذ 
+$admincount=mysqli_fetch_assoc($result);//بتجيب كل الصفوف
+?>
 
 
     <div class="container py-5">
@@ -8,11 +14,11 @@ require('header.php');
 
             <div class="col-md-4">
                 <div class="card text-white bg-info mb-3">
-                    <div class="card-header">Products</div>
+                    <div class="card-header">Admins</div>
                     <div class="card-body">
                         <div class="card-text d-flex justify-content-between align-items-center">
-                            <h5>233</h5>
-                          <a href="#" class="btn btn-light">Show</a>
+                            <h5><?= $admincount['admincount']?></h5>
+                          <a href="admins.php" class="btn btn-light">Show</a>
                         </div>
                     </div>
                 </div>
